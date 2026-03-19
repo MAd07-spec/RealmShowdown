2cbb083dceee101973c71408ce9f593f5a10e92c
MAd07-spec <mehedehassanamin1@gmail.com>
2026-03-19 00:12:55 +0000

    sgs

--- player_two.gd
+++ player_two.gd
@@ -3,7 +3,7 @@ var speed = 300.0
 var is_attacking = false
 var attack_damage = 10
 var jump_force = -1200.0
-var gravity_scale = 4.5
+var gravity_scale = 4.0
 @onready var sprite = $AnimatedSprite2D
 func _physics_process(delta):
 	
@@ -16,26 +16,30 @@ func _physics_process(delta):
 		velocity.y += gravity * delta * gravity_scale
 	else:
 		velocity.y = 0
+
 	# MOVEMENT
 	var direction = 0
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("p2_right"):
 		direction = 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("p2_left"):
 		direction = -1
 	velocity.x = direction * speed
 
 	# JUMP
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("p2_jump") and is_on_floor():
 		velocity.y = jump_force
 
 	move_and_slide()
+
 	# BOUNDARIES
-	#dadaaposition.x = clamp(position.x, -740, 740)
+	#position.x = clamp(position.x, -740, 740)
+
 	# ATTACK
-	if Input.is_action_just_pressed("attack") and not is_attacking:
+	if Input.is_action_just_pressed("p2_attack") and not is_attacking:
 		is_attacking = true
 		sprite.play("attack")
-		print("Attack hit! Damage:", attack_damage)
+		print("Player 2 Attack! Damage:", attack_damage)
+
 	# ANIMATIONS
 	if is_attacking:
 		if not sprite.is_playing():
@@ -46,11 +50,11 @@ func _physics_process(delta):
 		sprite.play("running")
 	else:
 		sprite.play("idle")
-	
-	# FLIP - face player 2 unless moving away
-	var p2 = get_node("/root/World/Player2")
+
+	# FLIP - face player 1 unless moving away
+	var p1 = get_node("/root/World/Player")
 	if direction == 0:
-		if p2.position.x < position.x:
+		if p1.position.x < position.x:
 			sprite.flip_h = true
 		else:
 			sprite.flip_h = false
@@ -59,4 +63,3 @@ func _physics_process(delta):
 			sprite.flip_h = false
 		elif direction == -1:
 			sprite.flip_h = true
-		
